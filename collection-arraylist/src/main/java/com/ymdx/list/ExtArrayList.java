9faee4e07089f78ef85f9aa49870dd0d69b50e6c
ymdx <y_m_d_x@163.com>
2020-01-15 18:02:59 +0800

    update README.md

---
+++ collection-arraylist/src/main/java/com/ymdx/list/ExtArrayList.java
@@ -0,0 +1,12 @@
+package com.ymdx.list;
+
+/**
+ * @ClassName: ExtArrayList
+ * @Description: 自定义ArrayList
+ * @Author: ymdx
+ * @Email: y_m_d_x@163.com
+ * @Date: 2020-01-15 17:53
+ * @Version: 1.0
+ **/
+public class ExtArrayList implements ExtList {
+}
---
+++ collection-arraylist/src/main/java/com/ymdx/list/ExtList.java
@@ -0,0 +1,12 @@
+package com.ymdx.list;
+
+/**
+ * @ClassName: ExtList
+ * @Description: 自定义List接口
+ * @Author: ymdx
+ * @Email: y_m_d_x@163.com
+ * @Date: 2020-01-15 17:53
+ * @Version: 1.0
+ **/
+public interface ExtList {
+}
